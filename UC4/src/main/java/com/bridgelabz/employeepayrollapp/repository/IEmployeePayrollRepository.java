9d5022fcb2f3da68c0866964b8639921f39234b9
SaiVarun107 <saivarunbitra107@gmail.com>
2025-05-25 00:30:34 +0530

    Delete UC4 directory

--- UC4/src/main/java/com/bridgelabz/employeepayrollapp/repository/IEmployeePayrollRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bridgelabz.employeepayrollapp.repository;
-
-import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IEmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {}
